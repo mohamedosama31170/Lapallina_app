2f4a61d00939ee56c0fdbf8a73cbf243b6ca09cb
mohamedosama31170 <mohamedosamakhalil31@gmail.com>
2026-02-11 13:24:15 +0200

    Add full project

---
+++ lib/app_constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+abstract class AppConstants {
+  static const String appTitle = 'La pallina';
+  static const String initialUrl = 'https://lapallina-store.com/';
+  static const String splashScreenImage = 'assets/app_icon.png';
+ static const Color primaryColor = Color.fromARGB(255, 23, 64, 60);
+
+
+
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'lapallina_app.dart';
+
+void main() {
+  runApp(const LapallinaApp());
+}
